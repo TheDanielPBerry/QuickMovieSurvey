d84d4e5f841ff081edd2ca2ed21de8980307f716
TheDanielPBerry <dberry@sagecomputing.com>
2024-09-30 23:23:43 -0400

    Add clemson favicon

--- app/index.php
+++ app/index.php
@@ -144,6 +144,7 @@ if(!empty($errors) || $_SERVER['REQUEST_METHOD'] !== 'POST') {
 <head>
 	<title>Movie Preferences | CPSC 8740</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 	<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
